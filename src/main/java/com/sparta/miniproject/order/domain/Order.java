a36f50518e0282ace0eac3fe38368560bff51342
sunsik17 <17sunsik@gmail.com>
2026-01-30 14:16:28 +0900

    init: create entity

---
+++ src/main/java/com/sparta/miniproject/order/domain/Order.java
@@ -0,0 +1,18 @@
+package com.sparta.miniproject.order.domain;
+
+import com.sparta.miniproject.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+}
---
+++ src/main/java/com/sparta/miniproject/product/domain/Product.java
@@ -0,0 +1,25 @@
+package com.sparta.miniproject.product.domain;
+
+import com.sparta.miniproject.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Long price;
+
+	@Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+	private Long stock;
+}
