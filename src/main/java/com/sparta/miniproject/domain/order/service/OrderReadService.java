efd00353b663b66006638199f87de106c23dd38f
sunsik17 <17sunsik@gmail.com>
2026-02-03 16:18:34 +0900

    feat: add order create function

--- src/main/java/com/sparta/miniproject/domain/order/service/OrderReadService.java
+++ src/main/java/com/sparta/miniproject/domain/order/service/OrderReadService.java
@@ -1,9 +1,6 @@
 package com.sparta.miniproject.domain.order.service;
 
-import com.sparta.miniproject.domain.order.dto.OrderDto;
-import com.sparta.miniproject.domain.order.entity.Order;
 import com.sparta.miniproject.domain.order.repository.OrderRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +10,4 @@ public class OrderReadService {
 
 	private final OrderRepository orderRepository;
 
-	public List<OrderDto> getAllOrders() {
-		List<Order> orders = orderRepository.findAll();
-	}
 }
